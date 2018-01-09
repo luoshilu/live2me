bc4acaa7119dce02a47a25eb5bc06c8efed5c181
luoshilu <978337784@qq.com>
2018-01-09 10:42:52 +0800

    sche文件重命名

---
+++ src/ngrx/action/sche.action.ts
@@ -0,0 +1,58 @@
+
+import { Action } from '@ngrx/store';
+import { Schedule } from '../../server/Utils';
+
+export const LOAD_SCHE = 'LOAD_SCHE'; // 获取日程列表
+export const ADD_SCHE = 'ADD_SCHE'; // 添加日程
+export const EDIT_SCHE = 'EDIT_SCHE'; // 编辑保存日程
+export const DEL_SCHE = 'DEL_SCHE'; // 删除日程列表
+export const FIND_SCHE = 'FIND_SCHE'; // 查询日程列表
+export const SCHE_SUCCESS = 'SCHE_SUCCESS'; // 日程操作成功
+export const SCHE_FAILDED = 'SCHE_FAILDED'; // 日程操作失败
+export const LOAD_SCHE_SUCCESS = 'LOAD_SCHE_SUCCESS'; // 获取成功
+export const LOAD_SCHE_FAILED = 'LOAD_SCHE_FAILED'; // 获取成功
+
+export class AddScheAction implements Action {
+    readonly type = ADD_SCHE;
+    constructor (public payload: Schedule){}
+}
+
+export class EditScheAction implements Action {
+    readonly type = EDIT_SCHE;
+    constructor (public payload: Schedule){}
+}
+
+export class DelScheAction implements Action {
+    readonly type = DEL_SCHE;
+    constructor (public payload: Schedule){}
+}
+
+export class LoadScheAction implements Action {
+    readonly type = LOAD_SCHE;
+    constructor (){}
+}
+
+export class LoadScheSuccessAction implements Action {
+    readonly type = LOAD_SCHE_SUCCESS;
+    constructor (public payload: Schedule[]){}
+}
+
+export class LoadScheFailedAction implements Action {
+    readonly type = LOAD_SCHE_FAILED;
+}
+
+export class FindScheAction implements Action {
+    readonly type = FIND_SCHE;
+    constructor (public payload: string){}
+}
+
+export class ScheSuccessAction implements Action {
+    readonly type = SCHE_SUCCESS;
+    constructor (){}
+}
+
+export class ScheFailedAction implements Action {
+    readonly type = SCHE_FAILDED;
+    constructor (){}
+}
+export type Actions = AddScheAction | LoadScheAction | EditScheAction | FindScheAction;
---
+++ src/ngrx/effect/sche.effect.ts
@@ -0,0 +1,98 @@
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/switchMap';
+// import { Database } from '@ngrx/db';
+
+import { Injectable } from '@angular/core';
+// import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
+// import { Http } from '@angular/http';
+import { DataStorage } from '../../server/dataStorage';
+import { Action } from '@ngrx/store';
+import { Actions, Effect } from '@ngrx/effects';
+
+import * as scheAction from '../action/sche.action';
+// import { Schedule } from '../../server/Utils';
+
+
+@Injectable()
+export class AuthEffects {
+
+    @Effect()
+    loadSche$: Observable<Action> = this.actions$
+        .ofType(scheAction.LOAD_SCHE)
+        // .startWith(new scheAction.LoadScheAction())
+        .map(res => res)
+        .mergeMap(() => {
+                // 获取日程列表
+                return this.data.getSchedules()
+                .then((res) => {
+                    if (res) {
+                        return new scheAction.LoadScheSuccessAction(res);
+                    } else {
+                        return new scheAction.LoadScheFailedAction();
+                    }
+                }).catch(() => new scheAction.LoadScheFailedAction())
+            }
+        );
+
+    @Effect()
+    delSche$: Observable<Action> = this.actions$
+        .ofType(scheAction.DEL_SCHE)
+        // .startWith(new scheAction.LoadScheAction())
+        .map(res => res.payload)
+        .mergeMap((res) => {
+                // 获取日程列表
+                return this.data.delSchedule(res.id)
+                .then((res) => {
+                    if (res) {
+                        return new scheAction.ScheSuccessAction();
+                    } else {
+                        return new scheAction.ScheFailedAction();
+                    }
+                }).catch(() => new scheAction.ScheFailedAction())
+            }
+        );
+
+    @Effect()
+    editSche$: Observable<Action> = this.actions$
+        .ofType(scheAction.EDIT_SCHE)
+        .map(res => res.payload)
+        .mergeMap((res) => {
+                // 编辑结果
+                return this.data.editSchedule(res)
+                .then((res) => {
+                    if (res) {
+                        return new scheAction.ScheSuccessAction();
+                    } else {
+                        return new scheAction.ScheFailedAction();
+                    }
+                }).catch(() => new scheAction.ScheFailedAction())
+            }
+        );
+    // 添加日程的action
+    @Effect()
+    addsche$: Observable<Action> = this.actions$
+        .ofType(scheAction.ADD_SCHE)
+        .map(res => res.payload)
+        .mergeMap((res) => {
+            return this.data.addSchedule(res)
+            .then((res) => {
+                if (res) {
+                    return new scheAction.ScheSuccessAction();
+                } else {
+                    return new scheAction.ScheFailedAction();
+                }
+            }).catch(() => new scheAction.ScheFailedAction())
+        }
+        );
+
+    constructor(
+        // private http: Http,
+        private actions$: Actions,
+        private data: DataStorage,
+        // private db: Database
+    ) {}
+}
